4cb6b6a4b603901a4e6a52878016283eeccdfac6
842mono <842mono@gmail.com>
2018-11-19 19:04:53 +0200

    kill animation

---
+++ Assets/3D Models/soldier3/callbackScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class callbackScript : MonoBehaviour
+{
+
+	public Vector3 elVector3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// void testTrans()
+	// {
+	// 	Debug.Log("enemy clickededed.");
+	// 	Debug.Log(this.transform.position);
+	// 	Debug.Log(elVector3);
+	// 	this.transform.position.Set(elVector3.x, elVector3.y, elVector3.z);
+	// 	Debug.Log(this.transform.position);
+	// }
+}
--- Assets/Animations/Character Animations/Soldier3/enemyCtrl.cs
+++ Assets/Animations/Character Animations/Soldier3/enemyCtrl.cs
@@ -6,6 +6,8 @@ public class enemyCtrl : MonoBehaviour
 {
 	public Animator walkAnimator;
 	public Animator positionAnimator;
+	// public callbackScript script;
+	public GameObject enemy;
 
 	// Use this for initialization
 	void Start () {
@@ -19,11 +21,13 @@ public class enemyCtrl : MonoBehaviour
 	void OnMouseDown()
 	{
 		Debug.Log("enemy clicked.");
-		Vector3 currentPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-		Debug.Log(currentPos);
+		Vector3 elTarget = new Vector3(enemy.transform.position.x, enemy.transform.position.y, enemy.transform.position.z);
+		Debug.Log(elTarget);
+		enemy.transform.position = elTarget;
 		walkAnimator.SetBool("dead", true);
-		positionAnimator.SetBool("dead", true);
-		this.transform.position.Set(currentPos.x, currentPos.y, currentPos.z);
-		Debug.Log(currentPos);
+		// positionAnimator.SetBool("dead", true);
+		positionAnimator.speed = 0;
+		// this.transform.position.Set(currentPos.x, currentPos.y, currentPos.z);
+		// Debug.Log(currentPos);
 	}
 }
